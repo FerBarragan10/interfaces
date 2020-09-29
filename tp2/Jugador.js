96a6b0459592a65c1b02e2e9d91175322a2c4917
FerBarragan10 <fer7_10@hotmail.com>
2020-09-29 20:43:24 -0300

    clase jugador

---
+++ tp2/Jugador.js
@@ -0,0 +1,21 @@
+class Jugador{
+    constructor(ctx, nombre, nroJugador){
+        this.ctx = ctx;
+        this.nombre = nombre;
+        this.nroJugador = nroJugador;
+    
+    }
+
+    getNombre(){
+        return this.nombre;
+    }
+
+    getNroJugador() {
+        return this.nroJugador;
+    }
+
+    setNombre(nombre){
+        this.nombre = nombre;
+    }
+
+}
