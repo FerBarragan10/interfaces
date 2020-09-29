c1fe110fd1f205869e23ea8658e1b460e61aa470
FerBarragan10 <fer7_10@hotmail.com>
2020-09-29 20:43:06 -0300

    clase ficha

---
+++ tp2/Ficha.js
@@ -0,0 +1,71 @@
+class Ficha{
+    constructor(x, y, color, jugador){
+        this.x = x;
+        this.y = y;
+        this.radio = 25;
+        this.jugador = jugador;
+        this.estado = '';
+        this.imagen = new Image();
+        this.getSrc(color)
+    }
+
+    getSrc(color) {
+        if(color === 'j1')
+            this.imagen.src = 'images/Fichaj1.png';
+        else if (color === 'j2')
+            this.imagen.src = 'images/Fichaj2.png';
+        else if (color === 'ranuras')
+            this.imagen.src = 'images/FichaVacia.png';
+    }
+
+    getJugador(){
+        if (this.jugador === 0)
+            return this.jugador;
+        else 
+            return this.jugador.getNroJugador();
+    }
+
+    getNombre() {
+        return this.jugador.getNombre();
+    }
+
+    getEstado() {
+        return this.estado;
+    }
+
+    setContext(ctx){
+        this.ctx = ctx;
+    }
+
+    setX(x) {
+        this.x = x;
+    }
+
+    setY(y) {
+        this.y = y;
+    }
+
+    setColor(color){
+        this.color = color;
+    }
+
+    setEstado(estado) {
+        this.estado = estado;
+    }
+
+    isClicked(x, y) {
+        var xLayer = x - this.x;
+        var yLayer = y - this.y;
+        return Math.sqrt(xLayer*xLayer + yLayer*yLayer) < this.radio;
+    }
+
+    dibujar(){
+        this.ctx.beginPath();
+        this.ctx.arc(this.x, this.y, this.radio, 0, Math.PI*2);
+        this.ctx.fillStyle = '#d2cf76';
+        this.ctx.fill();
+        this.ctx.closePath();
+        this.ctx.drawImage(this.imagen, this.x - this.radio - 6, this.y - this.radio - 6);
+    }
+}
+	
