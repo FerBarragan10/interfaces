f57d3bcc436304074a79c6efbe7d54fc6e1a264e
FerBarragan10 <fer7_10@hotmail.com>
2020-09-29 20:44:03 -0300

    clase main

---
+++ tp2/Main.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    var canvas = document.getElementById('tablero');
+    var iniciarJuego = document.getElementById('iniciar-juego');
+    var juego = new Juego(canvas);
+
+    iniciarJuego.onclick = function() {
+        juego = new Juego(canvas);
+        juego.prepareJuego();
+        initEvents();
+        document.getElementById('info-ganador').classList.add('oculto');
+        document.getElementById('info-empate').classList.add('oculto');
+    }
+
+    function initEvents() {
+        canvas.onmousedown = function (e) {
+            var x = e.layerX - e.currentTarget.offsetLeft;
+            var y = e.layerY - e.currentTarget.offsetTop;            
+            juego.isClickedFicha(x, y);
+        }
+        
+        canvas.onmousemove = function (e) {
+            var x = e.layerX - e.currentTarget.offsetLeft;
+            var y = e.layerY - e.currentTarget.offsetTop;   
+            if (juego.hayFichaClickeada())
+                juego.moveFicha(x, y);
+        }
+
+        canvas.onmouseup = function (e) {
+            var x = e.layerX - e.currentTarget.offsetLeft;
+            var y = e.layerY - e.currentTarget.offsetTop;
+            if (juego.hayFichaClickeada()){
+                juego.insertarFicha(x,y);    
+            }
+        };
+    }
+});
+
