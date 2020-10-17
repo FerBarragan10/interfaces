6f9a9b0fe7da3f3cc6b638f10e3051a66de6573c
FerBarragan10 <fer7_10@hotmail.com>
2020-10-16 21:39:46 -0300

    parallax, grilla de personajes y cuenta regresiva

---
+++ tp3/parallax.js
@@ -0,0 +1,16 @@
+let layer1=document.querySelector("#layer1");
+let layer2=document.querySelector("#layer2");
+let layerNube=document.querySelector("#layer2-2");
+let layer3=document.querySelector("#layer3");
+let text=document.querySelector("#text")
+
+window.addEventListener("scroll",function(e){
+    let value=window.scrollY;
+    layer1.style.top= value * 0.5 + 'px';
+    layer2.style.left= - value * 0.5 + 'px';
+    layerNube.style.left= value *0.5 +'px';
+    layer3.style.top= - value * 0.15 + 'px';
+    text.style.top=value *0.5 + 'px';
+
+}
+);
---
+++ tp3/sound.js
@@ -0,0 +1,5 @@
+window.addEventListener("DOMContentLoaded", event => {
+    const audio = document.querySelector("source");
+    audio.volume = 0.2;
+    audio.play();
+  });
