26ce1ce4e4b508f2542015ccf363e8707eda4bb6
FerBarragan10 <fer7_10@hotmail.com>
2020-10-18 12:17:15 -0300

    slider, menu hambuguesa y formulario

--- tp3/slider.js
+++ tp3/slider.js
@@ -1,42 +1,4 @@
-/*let imagenes=['images/tapa.jpg','images/dobby.jpg','images/escenaAragog.jpg','images/grafitis.jpg','images/basilisco.jpg'];
-let contador=0;
 
-
-function carrousel(contenedor){
-    contenedor.addEventListener('click', e=>{
-        let atras=document.querySelector('.atras');
-        let siguiente=document.querySelector('.siguiente');
-        let img=document.querySelector('.img');
-
-    
-        tgt=e.target;
-        img.src = imagenes[ contador++ ];
-        if(tgt==atras){
-            if(contador>0){
-                img.src=imagenes[contador - 1 ];
-                contador--;
-            }else{
-                img.src=imagenes[imagenes.length - 1 ];
-                contador=imagenes.length - 1;
-            }
-        }else if(tgt==siguiente){
-                if(contador<imagenes.length - 1){
-                    img.src=imagenes[contador + 1];
-                    contador++;
-                }else{
-                    img.src=imagenes[0];
-                    contador=0;
-                }
-
-        }
-    });
-}
-document.addEventListener('DOMContentLoaded',() => {
-    let contenedor=document.querySelector('.contenedor');
-    carrousel(contenedor);
-})
-
-*/
 window.onload = function () {
     // Variables
     const IMAGENES = [
@@ -46,13 +8,13 @@ window.onload = function () {
         'images/scenas/escenaAragog.jpg',
         'images/scenas/basilisco.jpg',
     ];
-    const TIEMPO_INTERVALO_MILESIMAS_SEG = 1000;
+    const TIEMPO_INTERVALO_MILESIMAS_SEG = 3000;
     let posicionActual = 0;
-    let $botonRetroceder = document.querySelector('#retroceder');
-    let $botonAvanzar = document.querySelector('#avanzar');
-    let $imagen = document.querySelector('#imagen');
-    let $botonPlay = document.querySelector('#play');
-    let $botonStop = document.querySelector('#stop');
+    let botonRetroceder = document.querySelector('#retroceder');
+    let botonAvanzar = document.querySelector('#avanzar');
+    let imagen = document.querySelector('#imagen');
+    let botonPlay = document.querySelector('#play');
+    let botonStop = document.querySelector('#stop');
     let intervalo;
 
     // Funciones
@@ -85,7 +47,7 @@ window.onload = function () {
      * Funcion que actualiza la imagen de imagen dependiendo de posicionActual
      */
     function renderizarImagen () {
-        $imagen.style.backgroundImage = `url(${IMAGENES[posicionActual]})`;
+        imagen.style.backgroundImage = `url(${IMAGENES[posicionActual]})`;
     }
 
     /**
@@ -94,10 +56,10 @@ window.onload = function () {
     function playIntervalo() {
         intervalo = setInterval(pasarFoto, TIEMPO_INTERVALO_MILESIMAS_SEG);
         // Desactivamos los botones de control
-        $botonAvanzar.setAttribute('disabled', true);
-        $botonRetroceder.setAttribute('disabled', true);
-        $botonPlay.setAttribute('disabled', true);
-        $botonStop.removeAttribute('disabled');
+        botonAvanzar.setAttribute('disabled', true);
+        botonRetroceder.setAttribute('disabled', true);
+        botonPlay.setAttribute('disabled', true);
+        botonStop.removeAttribute('disabled');
 
     }
 
@@ -107,17 +69,17 @@ window.onload = function () {
     function stopIntervalo() {
         clearInterval(intervalo);
         // Activamos los botones de control
-        $botonAvanzar.removeAttribute('disabled');
-        $botonRetroceder.removeAttribute('disabled');
-        $botonPlay.removeAttribute('disabled');
-        $botonStop.setAttribute('disabled', true);
+        botonAvanzar.removeAttribute('disabled');
+        botonRetroceder.removeAttribute('disabled');
+        botonPlay.removeAttribute('disabled');
+        botonStop.setAttribute('disabled', true);
     }
 
     // Eventos
-    $botonAvanzar.addEventListener('click', pasarFoto);
-    $botonRetroceder.addEventListener('click', retrocederFoto);
-    $botonPlay.addEventListener('click', playIntervalo);
-    $botonStop.addEventListener('click', stopIntervalo);
+    botonAvanzar.addEventListener('click', pasarFoto);
+    botonRetroceder.addEventListener('click', retrocederFoto);
+    botonPlay.addEventListener('click', playIntervalo);
+    botonStop.addEventListener('click', stopIntervalo);
     // Iniciar
     renderizarImagen();
 } 
