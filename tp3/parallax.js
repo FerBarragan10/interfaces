8fc3b9f6f203b8eb43675f3f65740e63f2a7622e
FerBarragan10 <fer7_10@hotmail.com>
2020-10-19 19:19:46 -0300

    efecto parallax horizontal

--- tp3/parallax.js
+++ tp3/parallax.js
@@ -3,6 +3,8 @@ let layer2=document.querySelector("#layer2");
 let layerNube=document.querySelector("#layer2-2");
 let layer3=document.querySelector("#layer3");
 let text=document.querySelector("#text")
+let layer5=document.querySelector("#layer5");
+let layer6=document.querySelector("#layer6");
 
 window.addEventListener("scroll",function(e){
     let value=window.scrollY;
@@ -10,7 +12,8 @@ window.addEventListener("scroll",function(e){
     layer2.style.left= - value * 0.5 + 'px';
     layerNube.style.left= value *0.3 +'px';
     layer3.style.top= - value * 0.15 + 'px';
-    text.style.top=value *0.5 + 'px';
-
+    text.style.top= value *0.5 + 'px';
+    layer5.style.left=  value * 0.5 + 'px';
+    layer6.style.left= value *0.2 +'px';
 }
 );
